424b2476d38a3154442df43559f81ef7f31223ba
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-08-14 18:12:03 +0545

    added feat : name of loggedIn user shown up

--- Frontend/src/components/messagebox/MessageBox.jsx
+++ Frontend/src/components/messagebox/MessageBox.jsx
@@ -3,13 +3,14 @@ import MessageList from "./MessageList";
 import MessageInput from "./MessageInput";
 import { TiMessages } from "react-icons/ti";
 import useConversation from "../../storeZustand/useConversation";
+import { useAuthContext } from "../../context/AuthContext";
 const MessageBox = () => {
-const {selectedConversation, setSelectedConversation} = useConversation()
-useEffect(()=>{
-  return () => setSelectedConversation(null) //cleanup function (unmount)
-},[setSelectedConversation])
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  useEffect(() => {
+    return () => setSelectedConversation(null); //cleanup function (unmount)
+  }, [setSelectedConversation]);
   return (
-    <div className="md:min-w-[450px] flex flex-col border ">
+    <div className="md:min-w-[450px] flex flex-col border-l ">
       {!selectedConversation ? (
         <NoChatSelected />
       ) : (
@@ -17,7 +18,10 @@ useEffect(()=>{
           <div className="bg-gray-700 px-3 mb-2 h-12 text-left">
             <span className="label-text ">
               To:
-              <span className="text-slate-300 font-bold "> {selectedConversation.fullName} </span>
+              <span className="text-slate-300 font-bold ">
+                {" "}
+                {selectedConversation.fullName}{" "}
+              </span>
             </span>
           </div>
           <MessageList />
@@ -31,10 +35,11 @@ useEffect(()=>{
 export default MessageBox;
 
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl font-semibold flex flex-col items-center gap-2">
-        <p>Welcome Saroj </p>
+        <p>Welcome 🖐️ {authUser.fullName} </p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-5xl text-center" />
       </div>
