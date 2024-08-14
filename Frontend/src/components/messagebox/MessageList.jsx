a050f2c2233b00ead7ab276bd5ea6892ba600596
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-08-14 16:26:25 +0545

    fetched all the messages

--- Frontend/src/components/messagebox/MessageList.jsx
+++ Frontend/src/components/messagebox/MessageList.jsx
@@ -1,19 +1,33 @@
-import Message from './Message';
+import { useEffect, useRef } from "react";
+import useGetMessage from "../../hooks/useGetMessage";
+import MessageSkeleton from "../skeletons/messageSkeleton";
+import Message from "./Message";
 
 const MessageList = () => {
+  const { messages, loading } = useGetMessage();
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    lastMessageRef.current?.scrollIntoView({ behavior: "smooth" },100);
+  }, [messages]); // Add messages as a dependency
 
   return (
-    <div className='px-4 flex-1 overflow-auto'>
-
-
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+    <div className="px-4 flex-1 overflow-auto">
+      {!loading &&
+        messages.map((message) => (
+          <div
+            key={message._id}
+            ref={lastMessageRef} // Set ref only on the last message
+          >
+            <Message message={message} />
+          </div>
+        ))}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start conversation</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default MessageList;
