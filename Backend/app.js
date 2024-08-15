3da84dfaa7402d5e4f99ec794a5df88bb761b27a
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-08-15 18:18:59 +0545

    on deployment stage

--- Backend/app.js
+++ Backend/app.js
@@ -1,34 +1,38 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-
-import cors from "cors";
-
 
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 import connectDB from "./db/connectdb.js";
-import {app, server} from './socket/socket.js';
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 8000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(express.static(path.resolve("D:/Chat app/Frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(resolve("D:/Chat app/Frontend/dist/index.html"));
 });
 
 server.listen(PORT, () => {
- connectDB()
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
